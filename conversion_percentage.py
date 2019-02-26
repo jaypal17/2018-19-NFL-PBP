672051010292ac23b5a893c8180f5dfc58f359ea
Justin Pallett <justpallett@gmail.com>
2019-02-26 15:30:56 -0500

    Update conversion_percentage.py

--- conversion_percentage.py
+++ conversion_percentage.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 07:39:50 2019
+
+@author: JDPallett
+"""
+
 import pandas as pd
 
 # original dataset
-df = pd.read_csv('reg_pbp_2018_mellon_dataset.csv')
-
-# sorted data
-df = df.sort_values(['posteam', 'qtr', 'down', 'first_down_rush', 'first_down_pass', 'first_down_penalty'],
-                    ascending=[True, False, False, True, True, True])
+df = pd.read_csv('reg_pbp_2018_CarMel_RMVCOLs.csv',dtype={'game_id':int})
 
 # Ravens VS Bills to keep it simple
 df = df[df['game_id'] == 2018090900]
 
-# sorted data
-#df = df.sort_values(['posteam', 'qtr', 'down', 'first_down_rush', 'first_down_pass', 'first_down_penalty'],
-    #ascending=[True, False, False, True, True, True])
+#which columns are we keeping
+
 
 # stored all series
 all_series_success = []
@@ -38,6 +40,16 @@ for down in range(1,5):
 
 # Loop through the all_series_success and count how many success there were
 for series in all_series_success:
+    #check for combo of down and distance in series, then add to count
+    if True:
+        if row['down'] >= 1 and row['yards_gained'] > 0:
+            pass
+    
+    #check for all_series fail to count how many fails there were
+    if True:
+       if row['first_down_rush'] == 0 and row['first_down_pass'] == 0 and row['first_down_penalty'] == 0:
+            all_series_fail.append(df.iloc[end_of_series:index + 1]) 
+    pass
 
-
-
+#print out success rate
+print(str(down)+'&' + str(distance)+ ': ', num_success, 'out of', num_success + num_fail)
